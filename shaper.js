9648c978c7ff312c95879aca1e728fb17b6fabb1
Marc Neudert <marc.neudert@gmail.com>
2017-08-30 19:23:37 +0200

    converts shapefiles to geojson

--- shaper.js
+++ shaper.js
@@ -1,1 +1,11 @@
-console.log('shape-y shape-y!');
+const mapshaper = require('mapshaper');
+
+const fileStatesProvinces = 'ne_10m_admin_1_states_provinces';
+const geoStatesProvinces  = 'geo/' + fileStatesProvinces + '.json';
+const shpStatesProvinces  = 'shp/' + fileStatesProvinces + '.shp';
+
+console.log('convert: shp => json');
+mapshaper.runCommands([
+  '-i ' + shpStatesProvinces,
+  '-o ' + geoStatesProvinces
+].join(' '), () => { console.log('convert: shp => json [done!]'); });
